e5e9728c3794440b9f6d9b700c6b364f75ac84f9
Vikas Yadav <talktovik@users.noreply.github.com>
2021-01-04 18:53:32 +0530

    part 2

--- Enhancing_superpowers/Arrays/nth_largest_or_smallest.py
+++ Enhancing_superpowers/Arrays/nth_largest_or_smallest.py
@@ -1,1 +1,14 @@
 #nthlargestorsmallest
+
+# so according to this question we have to write the logic so that we can find out the 
+#kth biggest number in the list
+
+
+a = [1,22,334,22,44,22,11,2223,2234,2234]
+
+k = int(input("Enter the Kth value to find in the array."))
+
+x = sorted(a)
+print(x)
+print(x[-k])
+
---
+++ Enhancing_superpowers/Arrays/thedutchflagproblem.py
@@ -0,0 +1,4 @@
+#thedutchflagproblem
+#  so here we have the array having three kinds of numbers and we have to sort them without using any
+#  sorting algorithm
+		
