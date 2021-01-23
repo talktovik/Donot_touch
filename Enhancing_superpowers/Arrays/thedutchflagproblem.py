20cedaa30c4f30f6a58b16f0c820e09356f533f1
Vikas Yadav <talktovik@users.noreply.github.com>
2021-01-24 01:11:48 +0530

    This is nothing I guess.

--- Enhancing_superpowers/Arrays/thedutchflagproblem.py
+++ Enhancing_superpowers/Arrays/thedutchflagproblem.py
@@ -1,4 +1,20 @@
 #thedutchflagproblem
 #  so here we have the array having three kinds of numbers and we have to sort them without using any
 #  sorting algorithm
-		
+a = [1,1,1,0,0,0,2,2,2,0,0,0,0,0]
+#we have to sort in in a manner. Without using the sorting technique.
+low = 0
+mid = 0
+high = len(a)-1
+while mid <= high:
+	if a[mid] == 0:
+		a[mid],a[low] = a[low],a[mid]
+		mid += 1
+		low += 1
+
+	elif a[mid] == 1:
+		mid += 1
+	else:
+		a[mid],a[high] = a[high],a[mid]
+		high -= high
+print(a)
